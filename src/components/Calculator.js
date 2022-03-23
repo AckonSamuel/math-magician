be01b6f003b4dea271d5283633b6a679819aadde
AckonSamuel <ackons433@gmail.com>
2022-03-23 11:11:48 -0700

    Add calculator components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const operators = ['÷', 'x', '-', '+', '='];
+const digits = ['AC', '+/-', '%', '7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.'];
+
 class Button extends Component {
     constructor(props) {
         super(props);
@@ -22,4 +25,23 @@ Button.propTypes = {
     buttons: PropTypes.string.isRequired,
 };
 
+class Calculator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
+    render() {
+        return (
+            <ul className="calc-container">
+                <li className='calc-surface'>0</li>
+                <li className='calc-board'>
+                    <Button buttons = {digits}/>
+                </li>
+                <Button buttons={operators} />
+            </ul>
+        )
+    }
+}
+
+export default Calculator;
