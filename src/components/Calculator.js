73d1990cd6b8515078fb7429b57d4eb765855ef0
AckonSamuel <ackons433@gmail.com>
2022-03-24 16:02:02 -0700

    Refactor code to enhance clean code

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Button from './ButtonSkeleton';
+import calculate from '../logic/calculate';
 
 const operators = ['÷', 'x', '-', '+', '='];
 const digits = ['AC', '+/-', '%', '7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.'];
@@ -7,20 +7,44 @@ const digits = ['AC', '+/-', '%', '7', '8', '9', '4', '5', '6', '1', '2', '3', '
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      next: '',
+      total: '',
+      operation: '',
+    };
+  }
+
+  btnClick = (btn) => {
+    this.setState((state) => calculate(
+      {
+        next: state.next,
+        total: state.total,
+        operation: state.operation,
+      },
+      btn,
+    ));
   }
 
   render() {
+    const { total, next } = this.state;
     return (
       <div className="calc-container">
-        <div className="calc-input">0</div>
+        <div className="calc-input">{next || total || '0'}</div>
         <div className="calc-keys">
-          <Button buttons={digits} />
-          <Button buttons={operators} />
+          <div className="digits">
+            {digits.map((btn) => <button onClick={() => this.btnClick(btn)} type="button" key="btn">{btn}</button>)}
+          </div>
+          <div className="operators">
+            {operators.map((btn) => (
+              <button onClick={() => this.btnClick(btn)} type="button" key={btn}>
+                {btn}
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default { Calculator, operators, digits };
+export default Calculator;
--- src/components/eventsStructure/events.js
+++ src/components/eventsStructure/events.js
@@ -1,47 +1,43 @@
-import React, { Component } from 'react';
-import calculate from '../../logic/calculate';
-import { operators, digits } from '../../components/Calculator';
-import { render } from '@testing-library/react';
+// import React, { Component } from 'react';
+// import calculate from '../../logic/calculate';
+// import { operators, digits } from '../../components/Calculator';
+// import { render } from '@testing-library/react';
 
-class Calculator extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            next: '',
-            total: '',
-            operation: '',
-        }
-    }
+// class Calculator extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             next: '',
+//             total: '',
+//             operation: '',
+//         }
+//     }
 
+// btnClick = (btn) => {
+//     this.setState((state) => calculate(
+//         {
+//             next: state.next,
+//             total: state.total,
+//             operation: state.operation,
+//         },
+//         btn,
+//     ));
+// }
 
-btnClick = (btn) => {
-    this.setState((state) => calculate(
-        {
-            next: state.next,
-            total: state.total,
-            operation: state.operation,
-        },
-        btn,
-    ));
-}
+// render() {
+//     const { total, next } = this.state;
+//     return (
+//         <div className="calc-body">
+//             <div className="calc-screen">
+//                 { next || total || '0' }
+//             </div>
+//             <div className="calc-keyboard">
 
-render() {
-    const { total, next } = this.state;
-    return (
-        <div className="calc-body">
-            <div className="calc-screen">
-                { next || total || '0' }
-            </div>
-            <div className="calc-keyboard">
-                <div className="calc-digits">{digits.forEach((btn) => <button onClick={ () => this.btnClick(btn)} type="button" key="btn">{ btn }</button>) }
-                </div>
-                <div className="calc-operators">
-                    { operators.forEach((item) => <button onClick={() => this.btnClick(btn)} type="button" key={btn}>{ btn } </button>)}
-                </div>
-            </div>
-        </div>
-    )
-}
-}
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+// }
 
-export default Calculator;
+// export default Calculator;
