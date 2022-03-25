1def030a4db1284883a0ab1684a9d55cdd7e1236
AckonSamuel <ackons433@gmail.com>
2022-03-25 06:02:48 -0700

    Hot fix linter suggestions

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,7 +5,7 @@ const operators = ['÷', 'x', '-', '+', '='];
 const digits = ['AC', '+/-', '%', '7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.'];
 
 const Calculator = () => {
-  const [ obj, calcObj ] = useState({
+  const [obj, calcObj] = useState({
     next: '',
     total: '',
     operation: '',
@@ -24,30 +24,30 @@ const Calculator = () => {
 
   const btnClick = (btn) => {
     const { total, next, operation } = calculate(obj, btn);
-    if(next === null && total === null){
+    if (next === null && total === null) {
       calcObj({ next, total: '', operation });
     } else {
       calcObj({ next, total, operation });
     }
   };
 
-    return (
-      <div className="calc-container">
-        <div className="calc-input">{next || total || '0'}</div>
-        <div className="calc-keys">
-          <div className="digits">
-            {digits.map((btn) => <button onClick={() => this.btnClick(btn)} type="button" key={btn}>{btn}</button>)}
-          </div>
-          <div className="operators">
-            {operators.map((btn) => (
-              <button onClick={() => this.btnClick(btn)} type="button" key={btn}>
-                {btn}
-              </button>
-            ))}
-          </div>
+  return (
+    <div className="calc-container">
+      <div className="calc-input">{next || total || '0'}</div>
+      <div className="calc-keys">
+        <div className="digits">
+          {digits.map((btn) => <button onClick={() => this.btnClick(btn)} type="button" key={btn}>{btn}</button>)}
+        </div>
+        <div className="operators">
+          {operators.map((btn) => (
+            <button onClick={() => this.btnClick(btn)} type="button" key={btn}>
+              {btn}
+            </button>
+          ))}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+};
 
 export default Calculator;
