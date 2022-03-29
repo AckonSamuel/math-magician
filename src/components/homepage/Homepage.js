1ad0dce06a630705e7c61ec30d3646a6f12029fd
AckonSamuel <ackons433@gmail.com>
2022-03-29 14:15:01 +0100

    add markup to homepage js

---
+++ src/components/homepage/Homepage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Homepage = () => (
+  <div className="Introduction">
+    <h1 className="title">LEGEND</h1>
+    <p>
+      Mathematics, a branch of Science
+      that incorporates structure,
+      order, and relation that has advanced from counting,
+      measuring and describing the shapes of objects.
+    </p>
+
+    <p>
+      Mathematics deals with logical reasoning and
+      quantitive calculation. Since the 17th century, it has been
+      an indispensable adjunct to the physical sciences and
+      technology, to the extent that it is considered the underlying
+      language of science.
+    </p>
+
+    <p>
+      Among the principal branches of mathematics are algebra,
+      analysis, arithmetic, combinatorics, Euclidean and non-Euclidean
+      geometrics, game theory, number theory, numerical analysis,
+      optimization, probability, set theory, statistics, topology,
+      and trigonometry
+    </p>
+
+    <p className="reference">
+        <a href="http://"
+    </p>
+  </div>
+);
